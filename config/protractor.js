d6a843c9477318817de676ecb109697770a2e012
caio1989 <kaiocoutrim@gmail.com>
2015-01-31 00:20:51 -0300

    fix protractor.js

--- config/protractor.js
+++ config/protractor.js
@@ -6,9 +6,9 @@ exports.config =
 		browser.driver.get('http://caio.dev:3000/#/auth');
 		browser.driver.findElement(by.id('entrar')).click();
 		browser.driver.findElement(by.id('login_field'))
-			.sendKeys('caiocutrim');
+			.sendKeys('blabla');
 		browser.driver.findElement(by.id('password'))
-			.sendKeys('02589874rock');
+			.sendKeys('blabla');
 		browser.driver.findElement(by.name('commit')).click();
 	}
 };
