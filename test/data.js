08d45499df02533c9798a6aba67e5a586d2611ce
caio1989 <kaiocoutrim@gmail.com>
2015-02-06 23:52:43 -0300

    add travis e2e configuration 3

--- test/data.js
+++ test/data.js
@@ -8,7 +8,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017/contatooh_test',
   function(err, db)
   {
     if (err) throw err;
-    db.dropDataBase(function(err)
+    db.dropDatabase(function(err)
     {
       if(err) return console.log(err);
       console.log('banco apagado com sucesso...');
