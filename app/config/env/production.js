87255312ed0cd2d993ae8dbf5e726c09f2613b1e
caio1989 <kaiocoutrim@gmail.com>
2015-02-09 18:43:16 -0300

    fixed travis

--- app/config/env/production.js
+++ app/config/env/production.js
@@ -1,9 +1,10 @@
+// para usar no openshift
 module.exports =
-{	env:'production'
-,	db: process.env.OPENSHIFT_MONGODB_URL + 'contactooh'
-,	clientID: process.env.CLIENT_ID
-,	clientSecret:process.env.CLIENT_SECRET
-,	port:process.env.OPENSHIFT_NODEJS_PORT
+{ env:'production'
+, db: process.env.OPENSHIFT_MONGODB_DB_URL + 'contactooh'
+, clientID: process.env.CLIENT_ID
+, clientSecret:process.env.CLIENT_SECRET
+, port:process.env.OPENSHIFT_NODEJS_PORT
 , address:process.env.OPENSHIFT_NODEJS_IP
 , domain:process.env.OPENSHIFT_APP_DNS
 };
